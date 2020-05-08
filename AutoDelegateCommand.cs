5335d72c0c2539e83e1061799aa38cd37b0b1ecc
markovcd <markovcd@gmail.com>
2020-05-08 12:36:58 +0200

    Update AutoDelegateCommand.cs

--- AutoDelegateCommand.cs
+++ AutoDelegateCommand.cs
@@ -3,7 +3,7 @@ using System.ComponentModel;
 using System.Linq.Expressions;
 using System.Windows.Input;
 
-namespace TestModule
+namespace Utilities
 {
   /*
    Similar to DelegateCommand that is commonly used for WPF but it automatically fires CanExecuteChanged.
